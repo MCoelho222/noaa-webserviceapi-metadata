c9716b8d17ba3563cb9e2f971b07ccd3153cfbb1
Marcelo Coelho <mcoelho2011@hotmail.com>
2025-03-13 12:24:05 -0300

    feat: all moved to classes

--- src/NOAAStations.py
+++ src/NOAAStations.py
@@ -3,9 +3,12 @@ from typing import Optional
 from request import Request
 
 
-class NOAAStations(Request):
+class NOAAStations():
     """Class for fetching the available stations from the NOAA API."""
-    def __init__(
+    def __init__(self) -> None:
+        self.data = None
+
+    async def fetch_stations(
         self,
         datasetid: Optional[str]=None,
         locationid: Optional[str]=None,
@@ -17,51 +20,27 @@ class NOAAStations(Request):
         sortfield: Optional[str]=None,
         sortorder: Optional[str]=None,
         limit: Optional[str]=1000,
-        offset: Optional[int]=0,
-        whitelist_path: Optional[str]=None,
-        whitelist_key: Optional[str]=None,
-        whitelist_value: Optional[str]=None) -> None:
-
-        super().__init__(whitelist_path, whitelist_key, whitelist_value)
-
-        self.datasetid = datasetid
-        self.locationid = locationid
-        self.datacategoryid = datacategoryid
-        self.datatypeid = datatypeid
-        self.extent = extent
-        self.startdate = startdate
-        self.enddate = enddate
-        self.sortfield = sortfield
-        self.sortorder = sortorder
-        self.limit = limit
-        self.offset = offset
-
-        self.params_dict = {
-            "datasetid": self.datasetid,
-            "locationid": self.locationid,
-            "datacategoryid": self.datacategoryid,
-            "datatypeid": self.datatypeid,
-            "extent": self.extent,
-            "startdate": self.startdate,
-            "enddate": self.enddate,
-            "sortfield": self.sortfield,
-            "sortorder": self.sortorder,
-            "limit": self.limit,
-            "offset": self.offset
-        }
-        self.q_string = self.build_query_string_from_dict(self.params_dict)
-        self.data = None
-
-    async def fetch(self) -> dict[str, str] | None:
+        offsets: Optional[list[int]]=[0]) -> dict[str, str] | None:
         """Download the available stations according to the specified parameters.
 
         Returns:
             Optional[dict[str, str]]: A dictionary with 'metadata' and 'results' keys or None.
         """
-        # Fetch station's endpoint
-        data = await self.get("stations", self.q_string)
+        params_dict = {
+            "datasetid": datasetid,
+            "locationid": locationid,
+            "datacategoryid": datacategoryid,
+            "datatypeid": datatypeid,
+            "extent": extent,
+            "startdate": startdate,
+            "enddate": enddate,
+            "sortfield": sortfield,
+            "sortorder": sortorder,
+            "limit": limit,
+        }
 
-        self.data = data
+        req = Request()
+        data = await req.get_with_offsets("stations", params_dict, offsets)
         return data
 
 
@@ -69,10 +48,10 @@ if __name__ == "__main__":
     import asyncio
 
     async def main():
-        noaa_stations = NOAAStations(datasetid='GSOM', locationid='FIPS:BR')
-        stations = await noaa_stations.fetch()
+        noaa_stations = NOAAStations()
+        stations = await noaa_stations.fetch_stations(datasetid='GSOM', locationid='FIPS:BR')
         if stations:
             print(stations["metadata"])
-            print(f"Total stations: {len(stations["results"])}")
+            print(f"Total stations downloaded: {len(stations["results"])}")
     
     asyncio.run(main())
